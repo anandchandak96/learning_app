61dc504e21931d9c7b5876796f00cacaaea8ac78
anand chandak <achandak@bestpeers.com>
2024-06-11 15:31:10 +0530

    2nd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
-    puts "show"
+    puts "show method"
   end
 
   # GET /posts/new
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
     
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created-------------" }
+        format.html { redirect_to post_url(@post), notice: "Post was successfully created----------------. hii this console.log" }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
